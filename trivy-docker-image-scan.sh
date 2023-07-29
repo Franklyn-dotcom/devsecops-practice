2275311092b0247bd8dab8793df8e01da0676f85
Franklyn-dotcom <mbanugofranklyn678@gmail.com>
2023-07-29 20:40:36 +0000

    update files and trivy file

---
+++ trivy-docker-image-scan.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+
+# create a variable called dockerImageName to print the image name of the dockerfile using the awk command
+dockerImageName=$(awk 'NR==1 {print $2}' Dockerfile)
+echo $dockerImageName
+
+docker run --rm -v $WORKSPACE:/root/.cache/ aquasec/trivy:0.43.1 -q image --exit-code 0 --severity HIGH --light $dockerImageName
+docker run --rm -v $WORKSPACE:/root/.cache/ aquasec/trivy:0.43.1 -q image --exit-code 1 --severity CRITICAL --light $dockerImageName
+
+    # Trivy scan result processing
+    exit_code=$?
+    echo "Exit Code : $exit_code"
+
+    # Check scan results
+    if [[ "${exit_code}" == 1 ]]; then
+        echo "Image scanning failed. Vulnerabilities found"
+        exit 1;
+    else
+        echo "Image scanning passed. No CRITICAL vulnerabilities found"
+    fi;
